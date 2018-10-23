eb974216d45f2c08dd62ced1aa7dde9a09ad0346
Jan Daniel Andersen (JAXX) <jaxx@oticon.com>
2018-10-23 13:31:34 +0200

    More cleanup.

--- HomeMade/XamarinClient/XamarinClient/MainPage.xaml.cs
+++ HomeMade/XamarinClient/XamarinClient/MainPage.xaml.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using CommonStandard;
-using Microsoft.AspNetCore.SignalR.Client;
 using Xamarin.Forms;
-using XamarinClient.Annotations;
 
 namespace XamarinClient
 {
@@ -25,75 +20,12 @@ namespace XamarinClient
 
         private void Login_Clicked(object sender, EventArgs e)
         {
-             var writer = new Action<string>(s => this.viewModel.SetMessage(s));
+            var writer = new Action<string>(s => this.viewModel.SetMessage(s));
 
             var client = new ConsoleClient(ServerUri, "android1", writer);
             client.ConnectAsync();
 
             this.viewModel.SetMessage("Connected ...");
-
-            //MessageTextBox.HeightRequest = 200;
-            //MessageTextBox.Text = "Connected";
-        }
-
-    }
-
-    public class MainPageViewModel : INotifyPropertyChanged
-    {
-        public string  Message { get; set; }
-
-        public void SetMessage(string message)
-        {
-            this.Message = message;
-            OnPropertyChanged(nameof(Message));
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
-
-    //internal class ConsoleClient
-    //{
-    //    private readonly HubConnection connection;
-    //    private readonly string login;
-    //    private readonly Action<string> writer;
-
-    //    public ConsoleClient(string serverUri, string login, Action<string> writer)
-    //    {
-    //        this.login = login;
-    //        this.writer = writer;
-    //        this.connection = new HubConnectionBuilder().WithUrl(serverUri).Build();
-    //    }
-
-
-    //    public async void ConnectAsync()
-    //    {
-    //        this.connection.Closed += async error =>
-    //        {
-    //            await Task.Delay(new Random().Next(0, 5) * 1000);
-    //            await this.connection.StartAsync();
-    //        };
-
-    //        this.connection.On<string>(
-    //            "displayMessage",
-    //            message =>
-    //            {
-    //                var formattedMessage = $"From Server: {message}";
-    //                this.writer(formattedMessage);
-    //            });
-
-    //        await this.connection.StartAsync();
-    //    }
-
-    //    public void SendMessage(string message)
-    //    {
-    //        this.connection.InvokeCoreAsync("Send", new object[] {this.login, message});
-    //    }
-    //}
-
 }
---
+++ HomeMade/XamarinClient/XamarinClient/MainPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using XamarinClient.Annotations;
+
+namespace XamarinClient
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public string Message { get; set; }
+
+        public void SetMessage(string message)
+        {
+            this.Message = message;
+            OnPropertyChanged(nameof(Message));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
